dd49fe7ce3bea783315f6cc9acc37eb68527e528
tgaribal <tgaribal@gmail.com>
2016-09-14 16:03:45 -0700

    finished stack functional-shared prompts

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    storage: {},
+    counter: 0
+  };
+
+  _.extend(instance, stackMethods);
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function () {
+    return this.counter;
+  },
+  push: function (value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+  pop: function () {
+    if (this.counter > 0) {
+      this.counter--; 
+    }
+    return this.storage[this.counter];
+  }
+};
 
 
