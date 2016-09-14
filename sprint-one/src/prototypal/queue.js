a27f21c7deea819eae6314a3286b34df844f6a1c
tgaribal <tgaribal@gmail.com>
2016-09-14 16:40:17 -0700

    finished prototypal class for stack, started for queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,20 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.counter = 0;
+  instance.storage = {};
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function() {
+  return this.counter;
+};
+
+queueMethods.enqueue = function (value) {
+  
+}
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.counter = 0;
+  instance.storage = {};
+  return instance;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function () {
+  return this.counter;
+};
+stackMethods.push = function (value) {
+  this.storage[this.counter] = value;
+  this.counter++;
+};
+stackMethods.pop = function () {
+  if (this.counter > 0 ) {
+    this.counter--;
+  }
+  return this.storage[this.counter];
+};
 
