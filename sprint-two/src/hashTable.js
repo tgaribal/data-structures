4ad89fc35fc5d5a978c4b9df11e762d6e781fa26
tgaribal <tgaribal@gmail.com>
2016-09-16 15:24:19 -0700

    Begin work on resizing functions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,7 +9,6 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   this._counter++;
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var buck = this._storage[index];
   if (this._storage[index] === undefined) {
     this._storage[index] = [];
   }
@@ -19,10 +18,10 @@ HashTable.prototype.insert = function(k, v) {
     }
   }
   this._storage[index].push([k, v]);
-  if (this._counter > this._limit * 0.75) {
-    this.resize(this._limit * 2);
-    console.log("oversized");
-  }
+  console.log(this._storage);
+  // if (this._counter > this._limit * 0.75) {
+  //   this.resize(this._limit * 2);
+  // }
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -48,23 +47,25 @@ HashTable.prototype.remove = function(k) {
   }
 };
 
+/*
 HashTable.prototype.resize = function(limit) {
   var newStorage = LimitedArray(limit);
   var oldStorage = this._storage;
-  this._storage = newStorage;
-  console.log(oldStorage);
-  console.log(this._storage);
 
-  for (var i = 0; i < oldStorage.length; i++) {
-    if (oldStorage[i].length) {
-      for (var j = 0; j < oldStorage[i].length; j++) {
-        this._storage.insert(oldStorage[i][j][0], oldStorage[i][j][1]);
+  console.log(oldStorage);
+  console.log(newStorage);
+
+  for (var key in oldStorage) {
+    console.log("oldStorage outer", oldStorage);
+    if (oldStorage[key] !== undefined) {
+      for (var j = 0; j < oldStorage[key].length; j++) {
+        newStorage.insert(oldStorage[key][j][0], oldStorage[key][j][1]);
       }
     }
   }
-  console.log(this._storage);
+  console.log(newStorage);
 };
-
+*/
 
 
 
