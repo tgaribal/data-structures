e93a4c20751460bae9adde37137613baf309931d
tgaribal <tgaribal@gmail.com>
2016-09-17 09:56:34 -0700

    Finish hasTable resizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,8 +7,8 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  this._counter++;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[index];
   if (this._storage[index] === undefined) {
     this._storage[index] = [];
   }
@@ -18,8 +18,8 @@ HashTable.prototype.insert = function(k, v) {
     }
   }
   this._storage[index].push([k, v]);
-  if (this._counter < this._limit * 0.75) {
-    //console.log("limit breached", this._storage);
+  this._counter++;
+  if (this._counter > this._limit * 0.75) {
     this.resize(this._limit * 2);
   }
 };
@@ -27,47 +27,53 @@ HashTable.prototype.insert = function(k, v) {
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage[index];
-  for (var i = 0; i < bucket.length; i++) {
-    if (bucket[i] === undefined) {
-      return undefined;
-    } else if (bucket[i][0] === k) {
-      return bucket[i][1];
+  if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i] === undefined) {
+        return undefined;
+      } else if (bucket[i][0] === k) {
+        return bucket[i][1];
+      }
     }
   }
   return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
-  this._counter--;
   var index = getIndexBelowMaxForKey(k, this._limit);
   for (var i = 0; i < this._storage[index].length; i++) {
     
     if (this._storage[index][i][0] === k) {
       delete this._storage[index][i];
+      this._counter--;
     }
+  }
+  if (this._counter < this._limit * 0.25) {
+    this.resize(this._limit / 2);
   }
 };
 
 HashTable.prototype.resize = function(limit) {
   var oldStorage = this._storage;
-  var newStorage = LimitedArray(limit);
-
-  console.log(newStorage);
+  this._counter = 0;
+  this._storage = LimitedArray(limit);
+  console.log(this._storage);
+  this._limit = limit;
 
   for (var key in oldStorage) {
-    //console.log(oldStorage[key][0][0]);
-    for (var i = 0; i < oldStorage[key].length; i++) {
-      var index = getIndexBelowMaxForKey(oldStorage[key][i][0], limit);
-      if (newStorage[index] === undefined) {
-        newStorage[index] = [];
-      }
-      for (var z = 0; z < newStorage[index].length; z++) {          
-        if (newStorage[index][z][0] === oldStorage[key][i][0]) {
-          newStorage[index][z][1] = oldStorage[key][i][1];
+    var bucket = oldStorage[key];
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i]) {
+        var tuple = bucket[i];
+        var index = getIndexBelowMaxForKey(tuple[0], limit);
+        if (this._storage[index] === undefined) {
+          this._storage[index] = [];
         }
-      }      
+        this._storage[index].push([tuple[0], tuple[1]]);
+        this._counter++;
+      }
     }
-  }s
+  }
 };
 
 
